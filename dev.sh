f993c855b6667bffb615466591a3e00272e20f44
stringcheeseguy <149595245+Prana-vvb@users.noreply.github.com>
2026-08-01 18:31:46 +0530

    cd: Init

--- dev.sh
+++ dev.sh
@@ -13,5 +13,15 @@ mv .content_build content
 
 trap 'echo -e "\nRestoring content directory..."; rm -rf content; mv .content_backup content; rm -rf .content_build; exit 0' INT TERM
 
+if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+    echo "Error: The current directory is not a git repository."
+    exit 1
+fi
+
+COMMIT_INFO=$(git log -1 --format="%h %ad, %s" --date=short)
+echo -e "\033[36mInjecting commit into footer:\033[0m $COMMIT_INFO"
+
+echo "const GIT_COMMIT = '$COMMIT_INFO';" > static/commit.js
+
 echo "Starting Anna server..."
-~/.scripts/build_site.sh "$@"
+./anna "$@"
